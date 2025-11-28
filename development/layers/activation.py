5ec9b64a160e4100d266c8c54d197fc50b1b5c5f
Mat-thias <busoyeom@gmail.com>
2025-11-28 13:49:10 +0100

    added activation

--- development/layers/activation.py
+++ development/layers/activation.py
@@ -46,11 +46,6 @@ class ReLU(Layer, nn.ReLU):
         Returns:
             Clamped output tensor according to current quantization mode
         """
-        # if self.is_compressed:
-        #     if self.is_quantized:
-        #         if hasattr(self, "input_quantize"):
-        #             input = self.input_quantize(input)
-                    # print(self.input_quantize.zero_point, self)
 
         return super().forward(input)
     
@@ -122,17 +117,6 @@ class ReLU(Layer, nn.ReLU):
         layer_param_def = ""
         layer_header = ""
 
-        # if self.is_quantized and hasattr(self, "input_quantize"):
-        #     assert self.input_quantize.scheme == QuantizationScheme.STATIC, f"{self.__class__.__name__} has a input_quantize and is not static quantize"
-        #     layer_def = f"{self.__class__.__name__} {var_name}({input_size}, *(int8_t*){var_name}_input_zero_point);\n"
-
-        #     param_header, param_def = convert_tensor_to_bytes_var(
-        #         self.input_quantize.zero_point, 
-        #         f"{var_name}_input_zero_point"
-        #     )
-        #     layer_header += param_header
-        #     layer_param_def += param_def
-        # else:
         scheme = None
         if self.is_quantized and hasattr(self, "input_quantize"):
             scheme = self.input_quantize.scheme
@@ -155,24 +139,23 @@ class ReLU(Layer, nn.ReLU):
 
 
 class ReLU6(Layer, nn.ReLU6):
-
+    """
+    Quantization-aware ReLU6 layer.
+    
+    Common in mobile-optimized networks (e.g., MobileNet).
+    
+    Quantization Logic:
+    Standard ReLU6: y = min(max(0, x), 6)
+    Quantized ReLU6: y_q = min(max(zero_point, x_q), six_point)
+    
+    Requires calculating 'six_point': the integer representation of 6.0
+    given the current scale and zero_point.
+    """
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
     def forward(self, input):
-        """Forward pass with quantization awareness
-        
-        Args:
-            input: Input tensor (float or quantized)
-            
-        Returns:
-            Clamped output tensor according to current quantization mode
-        """
-
-        if self.is_compressed:
-            if self.is_quantized:
-                if hasattr(self, "input_quantize"):
-                    input = self.input_quantize(input)
+        """Forward pass with quantization awareness"""
         return super().forward(input)
     
 
@@ -185,7 +168,7 @@ class ReLU6(Layer, nn.ReLU6):
         is_output_layer: bool = False, 
         metric: str = "l2"
     ):
-        # Nothing to do
+        # Propagate channel indices (Structured Pruning Support)
         super().init_prune_channel()
         return keep_prev_channel_index
 
@@ -193,17 +176,8 @@ class ReLU6(Layer, nn.ReLU6):
     def get_prune_channel_possible_hypermeters(self):
         return None
     
-    # def init_quantize(self, bitwidth, scheme, granularity, previous_output_quantize = None):
-    #     if scheme == QuantizationScheme.STATIC:
-    #         raise RuntimeError("Can not perform static quantization with ReLU6, fuse the model first!")
-    #         # setattr(self, "input_quantize", Quantize(
-    #         #     self, bitwidth, scheme, QuantizationGranularity.PER_TENSOR, scale_type=QuantizationScaleType.ASSYMMETRIC
-    #         # ))
-    #         return previous_output_quantize
-    
-
     def init_quantize(self, bitwidth, scheme, granularity, previous_output_quantize = None):
-
+        """Configures quantization for integer clamping."""
         if scheme == QuantizationScheme.STATIC:
             # raise RuntimeError("Can not perform static quantization with ReLU6, fuse the model first!")
             setattr(self, "input_quantize", Quantize(
@@ -243,28 +217,6 @@ class ReLU6(Layer, nn.ReLU6):
         layer_param_def = ""
         layer_header = ""
 
-        # if self.is_quantized and hasattr(self, "input_quantize"):
-        #     assert self.input_quantize.scheme == QuantizationScheme.STATIC, f"{self.__class__.__name__} has a input_quantize and is not static quantize"
-        #     layer_def = f"{self.__class__.__name__} {var_name}({input_size}, *(int8_t*){var_name}_input_zero_point, *(int8_t*){var_name}_input_six_point);\n"
-
-        #     param_header, param_def = convert_tensor_to_bytes_var(
-        #         self.input_quantize.zero_point, 
-        #         f"{var_name}_input_zero_point"
-        #     )
-        #     layer_header += param_header
-        #     layer_param_def += param_def
-
-        #     input_six_point = quantize_per_tensor_assy(torch.Tensor([6]), self.input_quantize.scale, self.input_quantize.zero_point)
-        #     param_header, param_def = convert_tensor_to_bytes_var(
-        #         input_six_point, 
-        #         f"{var_name}_input_six_point"
-        #     )
-        #     layer_header += param_header
-        #     layer_param_def += param_def
-
-        # else:
-
-
         scheme = None
         if self.is_quantized and hasattr(self, "input_quantize"):
             scheme = self.input_quantize.scheme
@@ -291,27 +243,6 @@ class ReLU6(Layer, nn.ReLU6):
 
         # layer_def = f"{self.__class__.__name__} {var_name}({input_size});\n"
 
-        layer_header += f"extern {self.__class__.__name__} {var_name};\n\n"
-        
-        return layer_header, layer_def, layer_param_def
-    
-
-
-        scheme = None
-        if self.is_quantized and hasattr(self, "input_quantize"):
-            scheme = self.input_quantize.scheme
-
-        if scheme != QuantizationScheme.STATIC:
-            layer_def = f"{self.__class__.__name__} {var_name}({input_size});\n"
-        else:
-            layer_def = f"{self.__class__.__name__} {var_name}({input_size}, *(int8_t*){var_name}_input_zero_point);\n"
-
-            param_header, param_def = convert_tensor_to_bytes_var(
-                self.input_quantize.zero_point, 
-                f"{var_name}_input_zero_point"
-            )
-            layer_header += param_header
-            layer_param_def += param_def
         layer_header += f"extern {self.__class__.__name__} {var_name};\n\n"
         
         return layer_header, layer_def, layer_param_def
