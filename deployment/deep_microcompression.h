fa111a04c4136fad5d96432696343fea37be8042
Mat-thias <busoyeom@gmail.com>
2025-11-28 19:51:19 +0100

    added the deep_microcompression.h

--- deployment/deep_microcompression.h
+++ deployment/deep_microcompression.h
@@ -1,9 +1,28 @@
+/*
+ * @file deep_microcompression.h
+ * @brief Main Header for the Deep Microcompression (DMC) Inference Engine.
+ *
+ * This file serves as the single entry point for the generated C library.
+ * It aggregates all layer definitions and the sequential container required
+ * to run the optimized models on bare-metal hardware.
+ *
+ * Usage in MCU Project:
+ * 1. Include this header: #include "deep_microcompression.h"
+ * 2. Include the generated model header: #include "my_model.h"
+ * 3. Call inference: my_model.forward(input_buffer);
+ *
+ * Reference:
+ * "DMC generates generic C code that performs consistently on non-ARM architectures...
+ * producing a standalone library."
+ */
+
 #ifndef DEEP_MICROCOMPRESSION
 #define DEEP_MICROCOMPRESSION
 
-
+// Core Container
 #include "models/sequential.h"
 
+// Layer Implementations
 #include "layers/activation.h"
 #include "layers/batchnorm.h"
 #include "layers/conv.h"
